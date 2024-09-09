2b76ad7281c0d3e9048c76b4064ae55a9746015f
narges <13.narges.kalhor.82@gmail.com>
2024-09-10 00:44:57 +0330

    add score

---
+++ script.js
@@ -0,0 +1,15 @@
+let score = prompt("plase enter your number:");
+if (score >= 90) {
+  console.log("+A");
+} else if (score >= 80) {
+  console.log("A");
+} else if (score >= 70) {
+  console.log("+B");
+} else if (score >= 60) {
+  console.log("B");
+} else if (score >= 50) {
+  console.log("C");
+} else score <= 50;
+{
+  console.log("D");
+}
